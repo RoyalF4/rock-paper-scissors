3a8241401f73a9bf079fc11da5351e849a29ed6a
Royal Finch <Royal2791@gmail.com>
2024-12-11 15:34:41 -0500

    add function to get computers choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3)
+  if(choice === 0) {
+    return 'rock';
+  } else if(choice === 1) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
