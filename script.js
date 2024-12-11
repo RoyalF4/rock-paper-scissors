7f7eb5ef702af61e47e13f63e86baecfa387096c
Royal Finch <Royal2791@gmail.com>
2024-12-11 15:58:54 -0500

    add function to play a round and play a whole game

--- script.js
+++ script.js
@@ -9,7 +9,53 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerChoice() {
+function getHumanChoice() {
   let choice = prompt('Enter rock, paper, or scissors:').toLocaleLowerCase().trim();
   return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+  if(computerChoice === humanChoice) {
+    console.log("It's a tie!")
+  } else if(computerChoice  === 'rock' && humanChoice === 'paper') {
+    console.log("You win! Paper beats Rock!");
+    humanScore++;
+  } else if(computerChoice === 'rock' && humanChoice === 'scissors') {
+    console.log("You lose! Rock beats Scissors!");
+    computerScore++;
+  } else if(computerChoice === 'paper' && humanChoice === 'rock') {
+    console.log("You lose! Paper beats Rock!");
+    computerScore++;
+  } else if(computerChoice === 'paper' && humanChioce === 'scissors') {
+    console.log("You win! Scissors beat Paper!");
+    humanScore++;
+  } else if(computerChoice === 'scissors' && humanChoice === 'rock') {
+    console.log("You win! Rock beats scissors!");
+    humanScore++;
+  } else if(computerChoice === 'scissors' && humanChoice === 'paper') {
+    console.log("You lose! Scissors beat Paper!");
+    computerScore++;
+  }
+}
+
+function playGame() {
+  for(let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    playRound(computerChoice, humanChoice);
+  }
+
+  if(computerScore === humanScore) {
+    console.log(`The final score was tied at ${computerScore} to ${humanScore}`);
+  } else if(computerScore > humanScore) {
+    console.log(`You lost! The score was ${computerScore} to ${humanScore}`)
+  } else {
+    console.log(`You won! The score was ${humanScore} to ${computerScore}`)
+  }
+}
+
+playGame();
